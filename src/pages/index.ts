7c0b64796bb67ae1a7dd00a9cf6c51319a414fc4
@aliaksandr.tsyhankou <aliaksandr.tsyhankou@britenet.com.pl>
2024-12-23 22:04:07 +0100

    add some changes for correct delpoy

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,3 @@
-export { LoginPage, RegisterPage, ResetPasswordPage } from './auth';
+export { LoginPage, RegisterPage, ResetPasswordPage } from './auth/index.ts';
 export { AboutPage, HomePage } from './common';
 export { DashboardPage } from './users';
